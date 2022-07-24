1d3a8fd5f77d8c4fe9fe092dee91c9d06c927026
misurse <felipecamposdavid@gmail.com>
2022-07-23 23:25:41 -0300

    feijao

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,49 @@
 const nomePokemon = document.querySelector('.nomePokemon');
-const tipoPokemon = document.querySelector('.tipoPokemon');
-const imagemPokemon = document.querySelector('.imagemPokemon');
-const imagemAnimadaPokemon = document.querySelector('.imagemAnimadaPokemon');
+const fotoPokemon = document.querySelector('.fotoPokemon');
+const fotoShinyPokemon = document.querySelector('.fotoShinyPokemon');
 const idPokemon = document.querySelector('.idPokemon');
+const tipoPokemon1 = document.querySelector('.tipoPokemon1');
+const tipoPokemon2 = document.querySelector('.tipoPokemon2');
+const imagemAnimadaPokemon = document.querySelector('.imagemAnimadaPokemon');
+const imagemAnimadaShinyPokemon = document.querySelector('.imagemAnimadaShinyPokemon');
 
-const botao = document.querySelector("#botao");
-const proximo = document.querySelector('#proximo');
-const anterior = document.querySelector('#anterior');
-var soma = 1;
-var menos = 1;
+const form = document.querySelector('.form');
+const inputPesquisa = document.querySelector('.inputPesquisa');
+const botaoProximo = document.querySelector('.botaoProximo');
+const botaoAnterior = document.querySelector('.botaoAnterior');
 
-let feijao = 1;
-
-botao.addEventListener("click", function(e) {
-    e.preventDefault();
-    const texto = document.querySelector("#texto");
-    const value = texto.value; 
-    dadosPokemon(value);
-});
-
-proximo.addEventListener("click", function(e) {
-    e.preventDefault();
-    dadosPokemon(feijao + 1);
-});
-
-anterior.addEventListener("click", function(e) {
-    e.preventDefault();
-    dadosPokemon(feijao - 1);
-});
-
-async function buscaPokemon(pokemon) {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
+const buscaPokemon = async (pokemon) => {
+    const respostaAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await respostaAPI.json();
     return data;
 }
 
-
 const dadosPokemon = async (pokemon) => {
     const data = await buscaPokemon(pokemon);
+    idPokemon.innerHTML = data['id'];
     nomePokemon.innerHTML = data.name;
-    tipoPokemon.innerHTML = data['types']['0']['type']['name'];
-    imagemPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_shiny']
-    imagemAnimadaPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
-    idPokemon.innerHTML = data['id']
+    fotoPokemon.src = data['sprites']['other']['dream_world']['front_default'];
+    tipoPokemon1.innerHTML = data['types']['0']['type']['name'];
+    tipoPokemon2.innerHTML = data['types']['1']['type']['name'];
+    imagemAnimadaPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    imagemAnimadaShinyPokemon.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_shiny'];
     feijao = data.id;
 }
 
+let feijao = 1;
 
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    dadosPokemon(inputPesquisa.value);
+    inputPesquisa.value = '';
+});
 
+botaoProximo.addEventListener('click', function(e) {
+    e.preventDefault();
+    dadosPokemon(feijao + 1);
+})
+
+botaoAnterior.addEventListener('click', function(e) {
+    e.preventDefault();
+    dadosPokemon(feijao - 1);
+})
